33c2bfa859b5a98f395cc0b5a0a5c19d73b11026
noebenitez <noebenitez_03@hotmail.com>
2021-04-04 18:23:53 -0300

    first commit

---
+++ src/test/java/com/utn/APINoticias/ApiNoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.APINoticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
